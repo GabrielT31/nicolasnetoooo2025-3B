bb5d1e541c407764a5bddb2c3ca168753b394f4c
Gabriel Henrique Rodrigues Teixeira <159148600+GabrielT31@users.noreply.github.com>
2025-03-14 09:30:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+let tamanhoFonteAtual = 1;
+
+aumentaFonteBotao.addEventListener('click', function() {
+    tamanhoFonteAtual += 0.1;
+    document.body.style.fontSize =`${tamanhoFonteAtual}rem`
+})
+
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+let tamanhoFonteAtuala = 1;
+
+diminuiFonteBotao.addEventListener('click', function() {
+    tamanhoFonteAtual -= 0.1;
+    document.body.style.fontSize =`${tamanhoFonteAtual}rem`
+})
